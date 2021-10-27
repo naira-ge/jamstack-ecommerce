ac9e76f53690e3cdc2c7e191023ab83d880b5712
Naira <nairagezhoyan@gmail.com>
2021-10-27 20:29:16 +0400

    connect to DB

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import db from './../../lib/db';
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+export default async function handler(req, res) {
+  await db.connect();
+  console.log('CONNECT');
+  await db.disconnect();
+  res.status(200).json({ name: 'John Doe' });
 }
